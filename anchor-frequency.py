3e2e05e58d3bb22fbf1e3a58e4e04471a9c38bfb
charlie42 <oninfrared@gmail.com>
2024-03-14 17:43:16 +0400

    first commit

---
+++ anchor-frequency.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+
+# Read in data
+df = pd.read_csv("data/facets_formatted.csv", header=0, index_col=0)
+
+print(df.describe()[["emotion_stress-management", 
+                     "cognition_attention-concentration", 
+                     "somatic-and-sensory-functions_sexual-identity-behavior",
+                     "daily-routines_screen-time",
+                     "cognition_thinking-speed"]].T)
+
+# Only keep columns with item responses
+df = df.drop(["Entry ID","Actor type","Subject ID","Study ID","Group ID","Time","Subject-Respondent Pair ID"], axis=1)
+# Drop binary cols
+binary_cols = [x for x in df.columns if "other-special-issues" in x]+["toileting"] 
+df = df.drop(binary_cols, axis=1)
+
+df = df.apply(pd.to_numeric, errors='coerce')
+
+# Shift all values from -0.5 to 0.5 (originally 0 to 1)
+df = df.apply(lambda x: x-0.5)
+
+means = [] # [ [name, average], [name, average] ]
+
+for col in df.columns:
+    col_name_left = col + "_LEFT"
+    col_name_right = col + "_RIGHT"
+    
+    only_left_values = -(df[df[col] <= 0][col])
+    only_right_values = (df[df[col] >= 0][col])
+    
+    print("DEBUG", col, np.mean(only_left_values), np.mean(only_right_values), np.mean(df[col]))
+
+    means.append([col_name_left, np.mean(only_left_values)])
+    means.append([col_name_right, np.mean(only_right_values)])
+
+anchor_col_name = "Anchor"
+mean_col_name = "Mean score (0-0.5)"
+df_means = pd.DataFrame(means, columns=[anchor_col_name, mean_col_name])
+
+df_means.dropna(how='any', inplace=True)
+df_means = df_means.sort_values(by=mean_col_name, ascending=False)
+df_means = df_means.set_index(anchor_col_name)
+
+df_means.to_csv("data/output/prevalent_anchors.csv")
+
+### Analyse binary cols separately
+df = pd.read_csv("data/facets_formatted.csv", header=0, index_col=0)[binary_cols]
+df.mean().T.sort_values().to_csv("data/output/prevalent_anchors_binary.csv")
---
+++ facets-preprocessing.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import numpy as np
+import json
+
+class FACETSFormatter():
+
+    def __init__(self):
+        self.json = None
+        self.df = None
+        self.item_names = None
+
+    def _parse_entries(self):
+        print("Available fields: ", self.json["assessment_response_list_anonymized"][0].keys())
+        rows = []
+        print("Entries in json: ", len(self.json["assessment_response_list_anonymized"]))
+        for entry in self.json["assessment_response_list_anonymized"]:
+            # Have to filter by gruop id here to get latest version of FACETS
+            #if entry["subject_group_id"] == "d71289b4-d743-475a-a7cc-166898569d62": 138
+            #if entry["subject_group_id"] == "ed5e3cf6-67f0-405c-be85-f33d3184ec3a": # 135
+            #if (entry["subject_group_id"] in ["e9ad5793-1862-4787-b0d8-d024c11519be", # 138
+            #                                  "d71289b4-d743-475a-a7cc-166898569d62" # 75
+            #]):
+            for section in entry["assessment_response_sections"]:
+                section_id = section["lisapedia_section_id"]
+                for item in section["assessment_response_items"]:
+                    item_id = item["lisapedia_item_id"]
+                    value = item["value"]
+                    rows.append([
+                        entry["subject_id"]+entry["last_updated_at"],
+                        entry["lisapedia_respondent_actor_id"],
+                        entry["subject_id"],
+                        entry["subject_group_id"],
+                        entry["last_updated_at"],
+                        entry["respondent_hash"],
+                        section_id,
+                        item_id,
+                        value    
+                    ])
+                
+        facets_df = pd.DataFrame(rows, columns=[
+            "Entry ID",
+            "Actor type", 
+            "Subject ID", 
+            "Group ID", 
+            "Time", 
+            "Subject-Respondent Pair ID",
+            "Section ID",
+            "Item ID", 
+            "Value"
+        ])
+        
+        self.df = facets_df
+
+    def _replace_item_ids_with_values(self):
+        # item_translation = pd.read_csv("facets_item_translation.csv", 
+        #                                sep=",", 
+        #                                index_col="assessment_item_id")
+        item_translation = pd.read_csv("resources/facets_item_translation.csv", 
+                                       sep=",")
+        item_translation_en = item_translation[item_translation["locale_code"] == "en"]
+        item_translation_en.set_index("assessment_item_id", inplace=True)
+        self.item_names = item_translation_en["slug"]
+
+        self.df["Item ID"] = self.df["Item ID"].map(
+            item_translation_en["slug"]).fillna(self.df["Item ID"])
+        
+    def _map_ids(self):
+        try:
+            id_mapping = pd.read_csv("data/id_mapping_facets.csv", index_col="subject_id")
+            self.df["Study ID"] = self.df["Subject ID"].map(
+            id_mapping["dislay_label"]
+        )
+        except FileNotFoundError:
+            self.df["Study ID"] = np.nan
+
+    def _transpose_items(self):
+        df_to_transpose = self.df.drop("Section ID", axis=1)
+        df_to_transpose = df_to_transpose.reset_index().pivot(index = [
+            "Entry ID", "Actor type", "Subject ID", "Study ID", "Group ID", "Time", "Subject-Respondent Pair ID"
+        ], columns = "Item ID", values = "Value").reset_index()
+
+        self.df = df_to_transpose
+
+    def _get_cat_names(self):
+        cats = []
+        for col in self.df.columns:
+            if "_" in col:
+                cat = col.split("_")
+                cats.append(cat)
+        cats = set(cat)
+        return cats
+
+    def transform(self, data):
+        self.json = data
+        self._parse_entries()
+        self._replace_item_ids_with_values()
+        self._map_ids()
+        print(self.df)
+        self._transpose_items()
+        return self.df
+        
+if __name__ == "__main__":
+    suger_data = json.load(open("data/suger.json", encoding='latin-1'), strict=False)
+    clichy_data = json.load(open("data/clichy.json", encoding='latin-1'), strict=False)
+
+    formatter = FACETSFormatter()
+    suger_df = formatter.transform(suger_data)
+    clichy_df = formatter.transform(clichy_data)
+
+    suger_df.to_csv("data/clichy_formatted.csv")
+    clichy_df.to_csv("data/suger_formatted.csv")
