8725936b6537775dd9056b72eae0738e42a1fd92
charlie42 <oninfrared@gmail.com>
2024-03-27 13:15:44 +0400

    irr for 1 admin, 2 admin, and mean between the two

--- irr.py
+++ irr.py
@@ -61,15 +61,22 @@ def get_icc_row(col, data, target, raters):
     ).set_index('Type').loc[f"ICC2k"][["ICC", "pval", "CI95%"]]
     return [col, icc["ICC"], icc["pval"], icc["CI95%"]]
 
-def prepare_data_for_irr(data, n_teachers_overlap, which_admin):
+def test_n_admin(df_grouped):
+    print("DEBUG n admin", print(df_grouped[["Subject ID", "Respondent Hash", "social-function_social-engagement"]]))
+
+def prepare_data_for_irr(data, facets_cols, n_teachers_overlap, which_admin):
     teachers, students = id_overlapping_teachers_and_students(data, n_teachers_overlap)
     data = data[(data["Subject ID"].isin(students)) & (data["Respondent Hash"].isin(teachers))]
 
     # Take first FACETS administration of the teacher, second, or mean of both
     if which_admin == "1":
-        print("DEBUG BEFORE", data[["Subject ID", "Time"]])
         data = data.groupby(["Subject ID", "Respondent Hash"]).first().reset_index()
-        print("DEBUG AFTER", data[["Subject ID", "Time"]])
+    elif which_admin == "2":
+        data = data.groupby(["Subject ID", "Respondent Hash"]).last().reset_index()
+    elif which_admin == "mean":
+        data = data.groupby(["Subject ID", "Respondent Hash"]).mean(numeric_only=True).reset_index()
+    else:
+        raise("Wrong which_admin value! Should be '1', '2', or 'mean'")
 
     return data
 
@@ -97,11 +104,26 @@ if __name__ == "__main__":
         "Entry ID", "Actor type", "Subject ID", "Study ID", "Group ID", "Time", "Respondent Hash", "Grade"
     ]]
 
-    clichy_for_irr = prepare_data_for_irr(clichy, n_teachers_overlap=3, which_admin="1")
-    suger_for_irr = prepare_data_for_irr(suger, n_teachers_overlap=4, which_admin="1")
+    # Use only first administration of each teacher
+    clichy_for_irr = prepare_data_for_irr(clichy, facets_cols, n_teachers_overlap=3, which_admin="1")
+    suger_for_irr = prepare_data_for_irr(suger, facets_cols, n_teachers_overlap=4, which_admin="1")
 
-    irr_icc(clichy_for_irr, facets_cols, filename_base="clichy")
-    irr_icc(suger_for_irr, facets_cols, filename_base="suger")
+    irr_icc(clichy_for_irr, facets_cols, filename_base="clichy_admin_1")
+    irr_icc(suger_for_irr, facets_cols, filename_base="suger_admin_1")
+
+    # Second administration
+    clichy_for_irr = prepare_data_for_irr(clichy, facets_cols, n_teachers_overlap=3, which_admin="2")
+    suger_for_irr = prepare_data_for_irr(suger, facets_cols, n_teachers_overlap=4, which_admin="2")
+
+    irr_icc(clichy_for_irr, facets_cols, filename_base="clichy_admin_2")
+    irr_icc(suger_for_irr, facets_cols, filename_base="suger_admin_2")
+
+    # Mean two administration
+    clichy_for_irr = prepare_data_for_irr(clichy, facets_cols, n_teachers_overlap=3, which_admin="mean")
+    suger_for_irr = prepare_data_for_irr(suger, facets_cols, n_teachers_overlap=4, which_admin="mean")
+
+    irr_icc(clichy_for_irr, facets_cols, filename_base="clichy_admin_mean")
+    irr_icc(suger_for_irr, facets_cols, filename_base="suger_admin_mean")
 
     # Get IRR for only good-TRR teachers (files created in trr.py)
     good_trr_clichy = pd.read_csv("data/clichy_good_trr.csv")
@@ -115,9 +137,10 @@ if __name__ == "__main__":
     if n_teachers >= 2:
         good_trr_clichy = prepare_data_for_irr(
             good_trr_clichy, 
+            facets_cols, 
             n_teachers_overlap=n_teachers-1 if n_teachers>2 else 2, # If more than two, then one less than all teachers 
             which_admin="1")
-        irr_icc(good_trr_clichy, facets_cols, filename_base="clichy_good_trr")
+        irr_icc(good_trr_clichy, facets_cols, filename_base="clichy_admin_1_good_trr")
     else:
         print("NOT ENOUGH TEACHERS WITH GOOD TRR IN CLICHY")
     
@@ -125,9 +148,10 @@ if __name__ == "__main__":
     if n_teachers >= 2:
         good_trr_suger = prepare_data_for_irr(
             good_trr_suger, 
+            facets_cols, 
             n_teachers_overlap=n_teachers-1 if n_teachers>2 else 2, # If more than two, then one less than all teachers 
             which_admin="1")
-        irr_icc(good_trr_suger, facets_cols, filename_base="suger_good_trr")
+        irr_icc(good_trr_suger, facets_cols, filename_base="suger_admin_1_good_trr")
     else: 
         print("NOT ENOUGH TEACHERS WITH GOOD TRR IN SUGER")
 
