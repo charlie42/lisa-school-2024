aa4dc678c44047395a25ffc2557944725a9e4646
charlie42 <oninfrared@gmail.com>
2024-03-27 12:47:58 +0400

    fix irr for teachers with good trr

--- irr.py
+++ irr.py
@@ -9,11 +9,13 @@ def find_all_sets_of_overlapping_students(data, n_teachers_overlap):
     # and students they rated
 
     teachers = list(data["Respondent Hash"].unique())
+    print("DEBUG teachers", teachers)
 
     overlaps = []
 
     import itertools
     teacher_sets = list(itertools.combinations(teachers, n_teachers_overlap))
+    print("DEBUG teacher sets", teacher_sets)
     for teacher_set in teacher_sets:
         sets_of_students = []
         for teacher in teacher_set:
@@ -43,6 +45,8 @@ def id_overlapping_teachers_and_students(data, n_teachers_overlap):
     # ID list of students and teachers with the largest overlap, for irr
 
     overlaps = find_all_sets_of_overlapping_students(data, n_teachers_overlap)
+
+    print("DEBUG overlaps", overlaps)
     teachers, students = find_largest_overlapping_student_set(overlaps)
 
     return teachers, students
@@ -57,9 +61,19 @@ def get_icc_row(col, data, target, raters):
     ).set_index('Type').loc[f"ICC2k"][["ICC", "pval", "CI95%"]]
     return [col, icc["ICC"], icc["pval"], icc["CI95%"]]
 
-def irr_icc(data, facets_cols, n_teachers_overlap, filename_base):
+def prepare_data_for_irr(data, n_teachers_overlap, which_admin):
     teachers, students = id_overlapping_teachers_and_students(data, n_teachers_overlap)
     data = data[(data["Subject ID"].isin(students)) & (data["Respondent Hash"].isin(teachers))]
+
+    # Take first FACETS administration of the teacher, second, or mean of both
+    if which_admin == "1":
+        print("DEBUG BEFORE", data[["Subject ID", "Time"]])
+        data = data.groupby(["Subject ID", "Respondent Hash"]).first().reset_index()
+        print("DEBUG AFTER", data[["Subject ID", "Time"]])
+
+    return data
+
+def irr_icc(data, facets_cols, filename_base):
     
     rows = []
     for col in facets_cols:
@@ -83,8 +97,11 @@ if __name__ == "__main__":
         "Entry ID", "Actor type", "Subject ID", "Study ID", "Group ID", "Time", "Respondent Hash", "Grade"
     ]]
 
-    irr_icc(clichy, facets_cols, n_teachers_overlap=3, filename_base="clichy")
-    irr_icc(suger, facets_cols, n_teachers_overlap=4, filename_base="suger")
+    clichy_for_irr = prepare_data_for_irr(clichy, n_teachers_overlap=3, which_admin="1")
+    suger_for_irr = prepare_data_for_irr(suger, n_teachers_overlap=4, which_admin="1")
+
+    irr_icc(clichy_for_irr, facets_cols, filename_base="clichy")
+    irr_icc(suger_for_irr, facets_cols, filename_base="suger")
 
     # Get IRR for only good-TRR teachers (files created in trr.py)
     good_trr_clichy = pd.read_csv("data/clichy_good_trr.csv")
@@ -93,7 +110,24 @@ if __name__ == "__main__":
     print(len(clichy["Respondent Hash"].unique()), len(good_trr_clichy["Respondent Hash"].unique()))
     print(len(suger["Respondent Hash"].unique()), len(good_trr_suger["Respondent Hash"].unique()))
 
-    irr_icc(clichy, facets_cols, n_teachers_overlap=2, filename_base="clichy_good_trr")
-    irr_icc(suger, facets_cols, n_teachers_overlap=3, filename_base="suger_good_trr")
-
+    # If enough teachres left:
+    n_teachers = len(good_trr_clichy["Respondent Hash"].unique())
+    if n_teachers >= 2:
+        good_trr_clichy = prepare_data_for_irr(
+            good_trr_clichy, 
+            n_teachers_overlap=n_teachers-1 if n_teachers>2 else 2, # If more than two, then one less than all teachers 
+            which_admin="1")
+        irr_icc(good_trr_clichy, facets_cols, filename_base="clichy_good_trr")
+    else:
+        print("NOT ENOUGH TEACHERS WITH GOOD TRR IN CLICHY")
+    
+    n_teachers = len(good_trr_suger["Respondent Hash"].unique())
+    if n_teachers >= 2:
+        good_trr_suger = prepare_data_for_irr(
+            good_trr_suger, 
+            n_teachers_overlap=n_teachers-1 if n_teachers>2 else 2, # If more than two, then one less than all teachers 
+            which_admin="1")
+        irr_icc(good_trr_suger, facets_cols, filename_base="suger_good_trr")
+    else: 
+        print("NOT ENOUGH TEACHERS WITH GOOD TRR IN SUGER")
 
